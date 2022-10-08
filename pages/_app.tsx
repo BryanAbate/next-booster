bd1cb6fc00a1339911320144b5d13726fe59115e
Bryan Abate <bryan.abate@gmail.com>
2022-10-08 18:04:34 +0200

    Add React query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,23 @@
 import '../styles/globals.css';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <Provider store={store}>
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Component {...pageProps} />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access */}
+      <Hydrate state={pageProps.dehydratedState}>
+        <Provider store={store}>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          <Component {...pageProps} />
+        </Provider>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
 
