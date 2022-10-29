2fe8da6686634c0a44897ff04e0c7414bc859452
Bryan Abate <bryan.abate@gmail.com>
2022-10-29 19:07:35 +0200

    Upgrade to Next 13

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from 'next';
+import { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
@@ -38,6 +38,8 @@ const Home: NextPage = () => (
 
         <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
           className={styles.card}
         >
           <h2>Deploy &rarr;</h2>
